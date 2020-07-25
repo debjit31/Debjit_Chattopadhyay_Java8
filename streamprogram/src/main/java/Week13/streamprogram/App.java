286067d8b5f7c2a4611289b9a42b34e3f70e470f
debjit31 <debjitchatterjee31@gmail.com>
2020-07-25 20:22:55 +0530

    task commit

---
+++ streamprogram/src/main/java/Week13/streamprogram/App.java
@@ -0,0 +1,32 @@
+package Week13.streamprogram;
+import java.util.Scanner;
+import java.util.stream.IntStream;
+public class App 
+{
+    	 public static void main(String[] args) 
+    	    {
+    	         
+    	        Scanner sc = new Scanner(System.in);
+    	         
+    	        System.out.println("Enter input string :");
+    	         
+    	        String inputString = sc.nextLine().replaceAll(" ", "").toLowerCase();
+    	        boolean isItPalindrome = IntStream.range(0, inputString.length()/2).
+    	                noneMatch(i -> inputString.charAt(i) != inputString.charAt(inputString.length() - i -1));
+    	         
+    	        if (isItPalindrome)
+    	        {
+    	            System.out.println(inputString+" is a palindrome");
+    	        }
+    	        else
+    	        {
+    	            System.out.println(inputString+" is not a palindrome");
+    	        }
+    	         
+    	        sc.close();
+    	    }
+    	
+
+    	
+    }
+
